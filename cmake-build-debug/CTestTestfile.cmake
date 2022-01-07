3eaafcff91f8dbbe4ae906dbfdcaa33d74382222
lapsizm <78648610+lapsizm@users.noreply.github.com>
2022-01-08 01:52:16 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kirill/new_aya/03-lab-03-shared-ptr-lapsizm
-# Build directory: /home/kirill/new_aya/03-lab-03-shared-ptr-lapsizm/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/kirill/new_aya/03-lab-03-shared-ptr-lapsizm/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/kirill/new_aya/03-lab-03-shared-ptr-lapsizm/CMakeLists.txt;55;add_test;/home/kirill/new_aya/03-lab-03-shared-ptr-lapsizm/CMakeLists.txt;0;")
